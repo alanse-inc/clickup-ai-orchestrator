b2b98cfa5621cd0041579b229eb1a34bfc5fed3f
Ryuya Ikeda <rikeda71@gmail.com>
2026-03-28 11:20:45 +0900

    fix: status handler の JSON エンコードエラーをログ出力する

--- internal/status/handler.go
+++ internal/status/handler.go
@@ -2,6 +2,7 @@ package status
 
 import (
 	"encoding/json"
+	"log/slog"
 	"net/http"
 	"time"
 
@@ -91,5 +92,7 @@ func (h *Handler) ServeHTTP(w http.ResponseWriter, _ *http.Request) {
 	}
 
 	w.Header().Set("Content-Type", "application/json")
-	_ = json.NewEncoder(w).Encode(resp)
+	if err := json.NewEncoder(w).Encode(resp); err != nil {
+		slog.Debug("status response encode error", "error", err)
+	}
 }
